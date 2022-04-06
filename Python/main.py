59b4c476219c017989125bf07e3bacf3e0b11c07
sarveshrane2000 <sarveshrane2000@gmail.com>
2022-04-06 14:52:47 +0530

    Added: Question related to binary search

--- Python/main.py
+++ Python/main.py
@@ -1,9 +1,9 @@
 # Make imports
-from binarySearch import binary_search
+from binarySearch import binary_search, Solution
 import time
 
 # Make test cases
-tests = [
+tests_binary_search = [
     {
         "input": {
             "array": [4,3,2,1],
@@ -34,14 +34,33 @@ tests = [
     }
 ]
 
+test_check_pos = [
+    {
+        "input": {
+            "array": [1,5,5,5,5,6,7],
+            "target": 5
+        },
+        "output": (1, 4)
+    }
+]
+
 # We can unpack a dictionary using the double start (**) before the dict variable.
 
 if __name__ == "__main__":
-    # Run the code here
     start = time.time() * 1000
+
+    # Run the code here
+    sol = Solution()
+
+    tests = tests_binary_search
+    # tests = test_check_pos
+
     for test in tests:
+        # result = sol.check_first_and_last(**test["input"])
         result = binary_search(**test["input"])
         output = test["output"]
         assert result == output, f"Failed for: {test, result, output}"
+
     end = time.time() * 1000
+
     print(f"All passed in {end - start}ms")
