7d552dec2c01c4117f482038a299ea9a68dfc179
sarveshrane2000 <sarveshrane2000@gmail.com>
2022-04-06 14:52:22 +0530

    Added: Question related to binary search

--- Java/src/BinarySearch.java
+++ Java/src/BinarySearch.java
@@ -19,10 +19,76 @@ public class BinarySearch {
         while (r >= l) {
             int mid = (l + r) / 2;
             String result = check_occurrence(array, target, mid);
-            if (result == "Found") return mid;
-            else if (result == "Right") l = mid + 1;
-            else if (result == "Left") r = mid - 1;
+            switch (result) {
+                case "Found":
+                    return mid;
+                case "Right":
+                    l = mid + 1;
+                    break;
+                case "Left":
+                    r = mid - 1;
+                    break;
+            }
         }
         return -1;
+    }
+}
+
+// Interesting question on binary search
+
+// Check the first and the last occurrence of the element in the list
+
+//  We will use binary search for solving this question.
+
+class Solution {
+    interface Condition {
+        String condition(int mid);
+    }
+
+    static int binarySearch(int left, int right, Condition condition) {
+        while (right >= left) {
+            int mid = (left + right) / 2;
+            String result = condition.condition(mid);
+            switch (result) {
+                case "found":
+                    return mid;
+                case "right":
+                    left = mid + 1;
+                    break;
+                case "left":
+                    right = mid - 1;
+                    break;
+            }
+        }
+        return -1;
+    }
+
+    static int checkFirst(ArrayList<Integer> array, int target) {
+        Condition c = mid -> {
+            if (array.get(mid) == target) {
+                if (mid > 0 && array.get(mid - 1) == target) return "left";
+                else return "found";
+            } else if (array.get(mid) < target) return "right";
+            else return "left";
+        };
+        return binarySearch(0, array.size() - 1, c);
+    }
+
+    static int checkLast(ArrayList<Integer> array, int target) {
+        Condition c = mid -> {
+            if (array.get(mid) == target) {
+                if (mid > 0 && array.get(mid + 1) == target) return "right";
+                else return "found";
+            } else if (array.get(mid) < target) return "right";
+            else return "left";
+        };
+        return binarySearch(0, array.size() - 1, c);
+    }
+
+    static ArrayList<Integer> checkFirstAndLast(ArrayList<Integer> array, int target) {
+        ArrayList<Integer> returnArray = new ArrayList<>();
+        returnArray.add(checkFirst(array, target));
+        returnArray.add(checkLast(array, target));
+        return returnArray;
     }
 }
--- Java/src/Main.java
+++ Java/src/Main.java
@@ -4,35 +4,72 @@ import java.util.HashMap;
 
 class TestCases {
     public ArrayList<HashMap<String ,Object>> getBinarySearchTests() {
-        ArrayList<HashMap<String, Object>> binarySearchTests = new ArrayList<HashMap<String, Object>>();
-        binarySearchTests.add(new HashMap<String ,Object>() {{
-            put("Input", new ArrayList<Integer>(Arrays.asList(6,5,3,2,1)));
+        ArrayList<HashMap<String, Object>> binarySearchTests = new ArrayList<>();
+
+        binarySearchTests.add(new HashMap<>() {{
+            put("Input", new ArrayList<>(Arrays.asList(6, 5, 3, 2, 1)));
             put("Target", 3);
             put("Output", 2);
         }});
-        binarySearchTests.add(new HashMap<String ,Object>() {{
-            put("Input", new ArrayList<Integer>(Arrays.asList(10,3,3,2,2,1)));
+        binarySearchTests.add(new HashMap<>() {{
+            put("Input", new ArrayList<>(Arrays.asList(10, 3, 3, 2, 2, 1)));
             put("Target", 3);
             put("Output", 1);
         }});
-        binarySearchTests.add(new HashMap<String ,Object>() {{
-            put("Input", new ArrayList<Integer>(Arrays.asList(7,5,4,2,1)));
+        binarySearchTests.add(new HashMap<>() {{
+            put("Input", new ArrayList<>(Arrays.asList(7, 5, 4, 2, 1)));
             put("Target", 3);
             put("Output", 3);
         }});
         return binarySearchTests;
+    }
+    public ArrayList<HashMap<String, Object>> getBinarySearchQuestionTests() {
+        ArrayList<HashMap<String, Object>> binarySearchQuestionTests = new ArrayList<>();
+
+        binarySearchQuestionTests.add(new HashMap<>() {{
+            put("Input", new ArrayList<>(Arrays.asList(1, 1, 1, 1, 2, 2, 3, 4, 5, 6)));
+            put("Target", 1);
+            put("Output", new ArrayList<>(Arrays.asList(0, 3)));
+        }});
+        binarySearchQuestionTests.add(new HashMap<>() {{
+            put("Input", new ArrayList<>(Arrays.asList(1, 2, 3, 3, 3, 4, 5, 6)));
+            put("Target", 3);
+            put("Output", new ArrayList<>(Arrays.asList(2, 4)));
+        }});
+        binarySearchQuestionTests.add(new HashMap<>() {{
+            put("Input", new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5)));
+            put("Target", 3);
+            put("Output", new ArrayList<>(Arrays.asList(2, 2)));
+        }});
+        binarySearchQuestionTests.add(new HashMap<>() {{
+            put("Input", new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5)));
+            put("Target", 6);
+            put("Output", new ArrayList<>(Arrays.asList(-1, -2)));
+        }});
+
+        return binarySearchQuestionTests;
     }
 }
 
 public class Main {
     public static void main(String[] args) {
         // Write testcases and execute code here.
+
         TestCases tc = new TestCases();
-        tc.getBinarySearchTests().forEach((test) -> {
-            int result = BinarySearch.run((ArrayList<Integer>)test.get("Input"), (int)test.get("Target"));
-            System.out.println(result == (int)test.get("Output"));
-            assert result == (int)test.get("Output") : "Test Failed";
-        });
-        System.out.println("Tests Passed!");
+//        ArrayList<HashMap<String, Object>> cases = tc.getBinarySearchTests();
+//        ArrayList<HashMap<String, Object>> cases = tc.getBinarySearchQuestionTests();
+//        cases.forEach((test) -> {
+//            int result = BinarySearch.run((ArrayList<Integer>)test.get("Input"), (int)test.get("Target"));
+//            System.out.println(result == (int)test.get("Output"));
+//            assert result == (int)test.get("Output") : "Test Failed";
+//            ArrayList<Integer> result = Solution.checkFirstAndLast((ArrayList<Integer>) test.get("Input"), (int) test.get("Target"));
+//            System.out.println(test.get("Output").equals(result) == true);
+//            if (test.get("Output").equals(result) != true) {
+//                throw new RuntimeException("Test Failed");
+//            }
+//            System.out.println(test.get("Output"));
+//            System.out.println(result);
+//        });
+//        System.out.println("Tests Passed!");
     }
 }
